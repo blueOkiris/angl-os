9ca4d9ee3379fb7575a0f49f62488063358bd7d9
Dylan Turner <dylantdmt@gmail.com>
2020-10-09 00:17:51 -0400

    Extracted test functions out of main function

--- src/Kernel.cpp
+++ src/Kernel.cpp
@@ -7,19 +7,14 @@
 using namespace angl;
 using namespace kernel;
 
-void angl::kernel::main() {
-    gdt::init();
-    idt::init();
-
-    io::terminal::init();
-    io::terminal::putStr("Welcome to ANGL OS!\nCreated by Dylan Turner\n");
-
+inline void testIdt() {
     io::terminal::putStr("\nTesting IDT...\n");
-    
     asm volatile ("int $0");
     asm volatile ("int $31");
     io::terminal::putStr("Done testing.\n");
+}
 
+inline void testIrqThroughTimer() {
     io::terminal::putStr("\nTesting IRQs...\n");
     io::terminal::putStr("  RTC test:\n");
     timer::init(50);
@@ -36,14 +31,26 @@ void angl::kernel::main() {
     timer::stop();
     io::terminal::putStr("  Done.\n");
     io::terminal::putStr("Done testing.\n");
+}
 
-    paging::init();
-
-    // Uncomment below to cause a page fault:
-    /*io::terminal::putStr("Forcing a page fault...\n");
+inline void testPageFault() {
+    io::terminal::putStr("Forcing a page fault...\n");
     uint32_t *ptr = (uint32_t *) 0xA0000000;
     uint32_t doPageFault = *ptr;
-    io::terminal::putInteger(doPageFault);*/
+    io::terminal::putInteger(doPageFault);
+}
+
+void angl::kernel::main() {
+    gdt::init();
+    idt::init();
+    paging::init();
+
+    io::terminal::init();
+    io::terminal::putStr("Welcome to ANGL OS!\nCreated by Dylan Turner\n");
+
+    //testIdt();
+    //testIrqThroughTimer();
+    //testPageFault(); // Uncomment this function to cause a page fault
 
     while(true);
 }
