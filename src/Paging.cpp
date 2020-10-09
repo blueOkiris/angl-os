f9c15815a3bd135929314c8763156e59ff1d4d0c
Dylan Turner <dylantdmt@gmail.com>
2020-10-09 00:42:13 -0400

    Changed global state to be within function

--- src/Paging.cpp
+++ src/Paging.cpp
@@ -9,31 +9,28 @@ using namespace kernel;
 extern "C" void load_page_directory(uint32_t *);
 extern "C" void enable_paging();
 
-static uint32_t pageDir_g[1024] __attribute__((aligned(4096)));
-static uint32_t primaryPageTable[1024] __attribute__((aligned(4096)));
-static uint32_t kernelPageTable[1024] __attribute__((aligned(4096)));
-
-static const uint32_t videoMemory = 0xB8000;
-
 void paging::init() {
+    uint32_t pageDir[1024] __attribute__((aligned(4096)));
     for(int i = 0; i < 1024; i++) {
         // Set everything to not present
-        pageDir_g[i] = 0x00000002; // Supervisor | Writeable | Not Present
+        pageDir[i] = 0x00000002; // Supervisor | Writeable | Not Present
     }
 
+    uint32_t primaryPageTable[1024] __attribute__((aligned(4096)));
     for(int i = 0; i < 1024; i++) {
         primaryPageTable[i] = (i * 0x1000) | 2; // Su | Read/Write | Not Present
     }
-    primaryPageTable[videoMemory / 0x1000] = videoMemory | 3; // Video present
+    primaryPageTable[0xB8000 / 0x1000] = 0xB8000 | 3; // Video present
 
+    uint32_t kernelPageTable[1024] __attribute__((aligned(4096)));
     for(int i = 0; i < 1024; i++) {
         kernelPageTable[i] = (0x4000000 + (i * 0x1000)) | 3; // Su | R/W | Pres
     }
 
-    pageDir_g[0] = reinterpret_cast<uint32_t>(primaryPageTable) | 3;
-    pageDir_g[1] = reinterpret_cast<uint32_t>(kernelPageTable) | 3;
+    pageDir[0] = reinterpret_cast<uint32_t>(primaryPageTable) | 3;
+    pageDir[1] = reinterpret_cast<uint32_t>(kernelPageTable) | 3;
 
     // Enable paging
-    load_page_directory(pageDir_g);
+    load_page_directory(pageDir);
     enable_paging();
 }
